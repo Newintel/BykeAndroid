610e52d59d927e6ef019fe5c16af382b4e1bd2fa
Victor Costa <victormouracarvalho@yahoo.com.br>
2022-12-08 10:41:08 +0100

    SignUp Updates

---
+++ app/src/main/java/com/example/bykeandroid/LoginFragment.kt
@@ -0,0 +1,36 @@
+package com.example.bykeandroid
+
+import androidx.fragment.app.Fragment
+
+//import com.example.mydatabaseapp.database.RegisterRepository
+
+class LoginFragment(): Fragment() {
+//    AndroidViewModel(application), Observable {
+//
+//    val users = repository.users
+//
+//    @Bindable
+//    val inputUsername = MutableLiveData<String>()
+//
+//    @Bindable
+//    val inputPassword = MutableLiveData<String>()
+//
+//    //Function triggered When the Login Button is Clicked, Via Binding.
+//    fun loginButton() {
+//        //-------Logic for the code
+//    }
+//
+//    fun signUP() {
+//        //navigate to the Register Fragment
+//    }
+//
+//
+//
+//    override fun removeOnPropertyChangedCallback(callback: Observable.OnPropertyChangedCallback?) {
+//    }
+//
+//    override fun addOnPropertyChangedCallback(callback: Observable.OnPropertyChangedCallback?) {
+//    }
+
+
+}
