ffa7655cb2b9f71afc0cf741fd210e9716cc0ffb
Newintel <franck.labracherie@gmail.com>
2023-01-08 15:18:32 +0100

    - Lecture/écriture fonctionnelle

--- app/src/main/java/com/example/bykeandroid/view/MainActivity.kt
+++ app/src/main/java/com/example/bykeandroid/view/MainActivity.kt
@@ -3,11 +3,7 @@ package com.example.bykeandroid.view
 import android.Manifest
 import android.annotation.SuppressLint
 import android.app.Activity
-import android.bluetooth.BluetoothAdapter
-import android.bluetooth.BluetoothGatt
-import android.bluetooth.BluetoothGattCallback
-import android.bluetooth.BluetoothManager
-import android.bluetooth.BluetoothProfile
+import android.bluetooth.*
 import android.bluetooth.le.ScanCallback
 import android.bluetooth.le.ScanResult
 import android.bluetooth.le.ScanSettings
@@ -15,14 +11,21 @@ import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.util.Log
 import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.example.bykeandroid.R
+import com.example.bykeandroid.data.Commands
+import com.example.bykeandroid.data.Coordinates
+import com.example.bykeandroid.data.parseCommand
 import com.example.bykeandroid.utils.MyDialog
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
 
 
 private const val ENABLE_BLUETOOTH_REQUEST_CODE = 1
@@ -31,6 +34,10 @@ private const val RUNTIME_PERMISSION_REQUEST_CODE = 2
 @SuppressLint("MissingPermission")
 class MainActivity : AppCompatActivity() {
     var macAdress : String? = null
+    lateinit var bluetoothGatt : BluetoothGatt
+    lateinit var comCharacteristic: BluetoothGattCharacteristic
+    var dataToSend: Array<ByteArray> = arrayOf()
+    var readData: ByteArray = byteArrayOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -60,6 +67,21 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun BluetoothGatt.printGattTable() {
+        if (services.isEmpty()) {
+            Log.i("printGattTable", "No service and characteristic available, call discoverServices() first?")
+            return
+        }
+        services.forEach { service ->
+            val characteristicsTable = service.characteristics.joinToString(
+                separator = "\n|--",
+                prefix = "|--"
+            ) { it.uuid.toString() }
+            Log.i("printGattTable", "\nService ${service.uuid}\nCharacteristics:\n$characteristicsTable"
+            )
+        }
+    }
+
     private val gattCallback = object : BluetoothGattCallback() {
         override fun onConnectionStateChange(gatt: BluetoothGatt, status: Int, newState: Int) {
             val deviceAddress = gatt.device.address
@@ -68,6 +90,10 @@ class MainActivity : AppCompatActivity() {
                 if (newState == BluetoothProfile.STATE_CONNECTED) {
                     Log.w("BluetoothGattCallback", "Successfully connected to $deviceAddress")
                     // TODO: Store a reference to BluetoothGatt
+                    bluetoothGatt = gatt
+                    Handler(Looper.getMainLooper()).post {
+                        bluetoothGatt.discoverServices()
+                    }
                 } else if (newState == BluetoothProfile.STATE_DISCONNECTED) {
                     Log.w("BluetoothGattCallback", "Successfully disconnected from $deviceAddress")
                     gatt.close()
@@ -76,6 +102,112 @@ class MainActivity : AppCompatActivity() {
                 Log.w("BluetoothGattCallback", "Error $status encountered for $deviceAddress! Disconnecting...")
                 gatt.close()
             }
+        }
+
+        override fun onServicesDiscovered(gatt: BluetoothGatt?, status: Int) {
+            gatt?.printGattTable()
+            val comService = gatt?.services?.last()?.characteristics?.first()?.service
+            val char = comService?.characteristics?.first()
+            if (char != null) {
+                comCharacteristic = char
+            }
+            read()
+        }
+
+        fun onRead(value: ByteArray) {
+            Log.i("onCharacteristicRead", "Read: ${value.contentToString()}")
+            if (readData.isNotEmpty()) {
+                readData += value
+            } else {
+                readData = value
+            }
+
+            val triple = parseCommand(readData)
+            if (triple == null) {
+                Log.w("BLE read", "Not a command")
+                return
+            }
+
+            val (command, length, info) = triple
+            if (command == Commands.NONE && length != 0){
+                Log.i("BLE read", "Incomplete data")
+                read()
+                return
+            }
+
+            readData = byteArrayOf()
+            when (command) {
+                else -> {
+                    Log.i("BLE read", "$command")
+                }
+            }
+        }
+
+        override fun onCharacteristicRead(
+            gatt: BluetoothGatt,
+            characteristic: BluetoothGattCharacteristic,
+            value: ByteArray,
+            status: Int
+        ) {
+            onRead(value)
+        }
+
+        // If version is >= 33
+        override fun onCharacteristicRead(
+            gatt: BluetoothGatt?,
+            characteristic: BluetoothGattCharacteristic?,
+            status: Int
+        ) {
+            if (characteristic != null) {
+                onRead(characteristic.value)
+            }
+        }
+
+        override fun onCharacteristicWrite(
+            gatt: BluetoothGatt?,
+            characteristic: BluetoothGattCharacteristic?,
+            status: Int
+        ) {
+            Log.i("onCharacteristicWrite", "Value: ${characteristic?.value}")
+            if (dataToSend.isNotEmpty()) {
+                ble_write(dataToSend[0])
+                dataToSend = dataToSend.drop(1).toTypedArray()
+            }
+        }
+    }
+
+    fun read() {
+        Log.i("read", "Reading")
+        bluetoothGatt.readCharacteristic(comCharacteristic)
+    }
+
+    fun write(command : Commands, info : Coordinates? = null) {
+        if (command.has_info() && info == null) {
+            throw Exception("Command has info but no info was provided")
+        }
+        val infoString = info?.let { Json.encodeToString(it) }.orEmpty()
+        val infoData = infoString.toByteArray()
+        val data = byteArrayOf(command.getCode(), infoData.size.toByte(), *infoData)
+
+
+        if (data.size > 20) {
+            for (i in data.indices step 20) {
+                dataToSend += data.sliceArray(i until minOf(i + 20, data.size))
+            }
+            ble_write("".toByteArray())
+        } else {
+            ble_write(data)
+        }
+    }
+
+    private fun ble_write(data : ByteArray) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            bluetoothGatt.writeCharacteristic(comCharacteristic, data, BluetoothGattCharacteristic.WRITE_TYPE_DEFAULT)
+        } else {
+            Log.i("write", "Must use deprecated write (API < 33)")
+            comCharacteristic.value = data
+            comCharacteristic.writeType = BluetoothGattCharacteristic.WRITE_TYPE_DEFAULT
+            bluetoothGatt.writeCharacteristic(comCharacteristic)
         }
     }
 
